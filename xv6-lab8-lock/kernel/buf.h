7a2b70f051405f87cb7bfbc93045e324d11c4811
Angeldoo <87635769+Angeldoo@users.noreply.github.com>
2022-08-11 00:55:41 +0800

    Add files via upload

---
+++ xv6-lab8-lock/kernel/buf.h
@@ -0,0 +1,12 @@
+struct buf {
+  int valid;   // has data been read from disk?
+  int disk;    // does disk "own" buf?
+  uint dev;
+  uint blockno;
+  struct sleeplock lock;
+  uint refcnt;
+//  struct buf *prev; // LRU cache list - lab8-2
+  struct buf *next;     // hash list
+  uchar data[BSIZE];
+  uint timestamp;   // the buf last using time - lab8-2
+};
---
+++ xv6-lab8-lock/kernel/spinlock.h
@@ -0,0 +1,12 @@
+// Mutual exclusion lock.
+struct spinlock {
+  uint locked;       // Is the lock held?
+
+  // For debugging:
+  char *name;        // Name of lock.
+  struct cpu *cpu;   // The cpu holding the lock.
+#ifdef LAB_LOCK
+  int nts;  // the number of times the loop failed to set the lock
+  int n;    // the count of calls to acquire for the lock
+#endif
+};
